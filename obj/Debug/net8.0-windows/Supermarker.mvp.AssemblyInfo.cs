bd4a92fda7cd50fdbbc44a26e8cfdc4824268bc8
Evelyn Pulgarin Gomez <evelynggomez9080@gmail.com>
2024-10-12 11:12:44 -0500

    Pay Mode View

--- obj/Debug/net8.0-windows/Supermarker.mvp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Supermarker.mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarker.mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41bb9f9db534b555f53248c40d7baeb8775de3a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarker.mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarker.mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
